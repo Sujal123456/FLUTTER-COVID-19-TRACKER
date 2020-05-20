701e93a3def2446f9617429d3d3a30d42d1a92a5
Sujal123456 <59948839+Sujal123456@users.noreply.github.com>
2020-05-20 00:33:34 -0700

    Add files via upload

---
+++ datasource.dart
@@ -0,0 +1,24 @@
+import 
+Color primaryBlack=Color(0xff202c3b);
+class DataSource {
+  static String quote="Nothing in life is ti be feared,i is only to be understood"
+  static List questionAnswers=[
+{
+  "question":"What is CoronaVirus?"
+  "answer":
+  "Cornavirus are largely "
+},
+{
+"question":"What is COVID-19?"
+"answer":
+""
+},
+
+}
+
+
+
+
+  ]
+  
+}
