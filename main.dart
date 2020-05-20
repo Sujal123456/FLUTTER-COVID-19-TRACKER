ad78cb4af78a0dc100f54c7263fa54c1825f2276
Sujal123456 <59948839+Sujal123456@users.noreply.github.com>
2020-05-20 00:32:42 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart'
+void main(){
+    runApp MaterailApp()
+    debugShowCheckModeBannner: false
+    theme:ThemeData
+    fontfamily: 'Circular',
+    primaryColor: primaryBlack
+    ),
+    home:Home(),
+};
+}
